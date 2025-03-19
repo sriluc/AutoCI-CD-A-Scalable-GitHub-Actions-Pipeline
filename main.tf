e805e526c39aab72b2f2937ac86e7bafb08a6e5d
srikanth basimsetty <srikanth.devops03@gmail.com>
2025-03-19 16:40:07 +0000

    Added monitoring with Prometheus and Grafana

---
+++ main.tf
@@ -0,0 +1,11 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+}
+
+# Commit 5 - 2025-03-19T16:40:07
